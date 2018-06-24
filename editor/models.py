2f5f3481af52cdc8236b62dc1b6c3d5a12439a8d
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-06-24 15:27:57 -0300

    Add models

--- editor/models.py
+++ editor/models.py
@@ -1,2 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    models.DateTimeField('date published')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Scene(models.Model):
+    name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('creation date')
+    user = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+class GameObject(models.Model):
+    name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('creation date')
+    user = models.ForeignKey(Scene, on_delete=models.CASCADE)
+
 
