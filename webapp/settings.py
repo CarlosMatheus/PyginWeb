105c6da644a90e3312e3895797f612905405ddda
Unknown <aloysiogl@gmail.com>
2018-06-28 06:53:01 -0300

    new changes

--- webapp/settings.py
+++ webapp/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
