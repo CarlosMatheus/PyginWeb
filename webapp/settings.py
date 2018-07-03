0a4d51290420f29f45c5a626ea630c5807066440
aloysio <aloysiogl@gmail.com>
2018-07-03 00:14:42 -0300

    chaged domain

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'qtkgidfxb!frm2%h%s&(o@pam*$ovzwtb(&#3gp!695j1=%0p7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pyginweb.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
